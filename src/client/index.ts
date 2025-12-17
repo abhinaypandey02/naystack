d79cd49dc6a30143be7955978d2fed890a9633da
Abhinay Pandey <abhinay.pandey@springy.se>
2025-12-17 17:03:08 +0530

    more utils for cleaner integration

--- src/client/index.ts
+++ src/client/index.ts
@@ -2,5 +2,6 @@ export * from "../auth/email/client";
 export * from "../auth/instagram/client";
 export * from "../file/client";
 export * from "../graphql/client";
+export * from "../graphql/server";
 export * from "./hooks";
 export * from "./seo";
--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,5 +1,4 @@
 export { GQLError } from "./errors";
 export { initGraphQLServer } from "./init";
-export * from "./server";
 export type { AuthorizedContext, Context } from "./types";
 export { field, FieldLibrary, query, QueryLibrary } from "./utils";
