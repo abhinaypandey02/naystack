6dd8bcfa8c97db15c5bb5ccc3c57762dbe609872
Abhinay Pandey <abhinay.pandey@springy.se>
2025-12-27 12:02:55 +0530

    bug fix

--- src/graphql/client.tsx
+++ src/graphql/client.tsx
@@ -14,7 +14,12 @@ import {
 } from "@apollo/client-integration-nextjs";
 import type { TypedDocumentNode } from "@graphql-typed-document-node/core";
 import { useToken } from "naystack/auth/email/client";
-import { PropsWithChildren, useCallback, useEffect, useState } from "react";
+import React, {
+  PropsWithChildren,
+  useCallback,
+  useEffect,
+  useState,
+} from "react";
 
 export const getApolloWrapper = (
   endpoint: string,
