c1a0a60c2117d86459193159966b80bf54b478b7
Abhinay Pandey <abhinay.pandey@springy.se>
2026-01-17 18:18:08 +0530

    type

--- src/graphql/index.ts
+++ src/graphql/index.ts
@@ -1,4 +1,10 @@
 export { GQLError } from "./errors";
 export { initGraphQLServer } from "./init";
 export type { AuthorizedContext, Context } from "./types";
-export { field, FieldLibrary, query, QueryLibrary } from "./utils";
+export {
+  field,
+  FieldLibrary,
+  query,
+  QueryLibrary,
+  type ResponseType,
+} from "./utils";
