e24595dd1eafcb6efba4b36c912558638ac566d8
Abhinay Pandey <abhinay.pandey@springy.se>
2026-01-19 20:50:41 +0530

    fix

--- src/auth/index.ts
+++ src/auth/index.ts
@@ -1,4 +1,4 @@
-export { checkAuthStatus, getEmailAuthRoutes } from "./email";
+export { checkAuthStatus, getContext, getEmailAuthRoutes } from "./email";
 export { initGoogleAuth } from "./google";
 export { initInstagramAuth } from "./instagram";
 export { getRefreshToken } from "./utils/token";
